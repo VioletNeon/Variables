bfaf6ff5a5fe1a9a295eebbbeaf584856e86cd3c
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-18 23:50:01 +0300

    Task-5: Defined and changed variable frog

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,17 @@ public class Main {
 
         friend = friend / 7;
         System.out.println("friend: " + friend);
+
+        var frog = 3.5;
+        System.out.println("frog: " + frog);
+
+        frog = frog * 10;
+        System.out.println("frog: " + frog);
+
+        frog = frog / 3.5;
+        System.out.println("frog: " + frog);
+
+        frog = frog + 4;
+        System.out.println("frog: " + frog);
     }
 }
